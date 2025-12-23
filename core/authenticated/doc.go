1aa4ff0f391f36b3a3e1982a5e4518a01f19d450
megalamo <previty20@gmail.com>
2025-12-23 15:50:23 -0500

    Create doc.go

---
+++ core/authenticated/doc.go
@@ -0,0 +1,7 @@
+/*
+Authenticated public state, stored in cookies, authenticated using paseto v4.public
+
+See the section about v4.public:
+https://github.com/paseto-standard/paseto-spec/tree/master/docs/01-Protocol-Versions
+*/
+package authenticated
