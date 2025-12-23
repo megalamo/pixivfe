ddc0a8a848b6fda248a66c19831090261ee91210
megalamo <previty20@gmail.com>
2025-12-23 15:56:20 -0500

    Create doc.go

---
+++ core/pixivision/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2023 - 2025, VnPower and the PixivFE contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/*
+Package pixivision provides pixivision functionality.
+*/
+package pixivision
