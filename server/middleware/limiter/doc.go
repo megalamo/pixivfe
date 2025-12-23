a5ae3960f8c3c416742aa9fd9177107af4f345f7
megalamo <previty20@gmail.com>
2025-12-23 15:21:52 -0500

    Create doc.go

---
+++ server/middleware/limiter/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2023 - 2025, VnPower and the PixivFE contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/*
+Package limiter is a middleware that enforces rate limiting and bot detection for HTTP requests.
+*/
+package limiter
