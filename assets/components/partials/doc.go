d60a1187ef38ebfc50da1a9b86de2ab4b1fdfced
megalamo <previty20@gmail.com>
2025-12-23 21:44:48 -0500

    Add files via upload

---
+++ assets/components/partials/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2023 - 2025, VnPower and the PixivFE contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/*
+Package partials holds components that would otherwise be included under fragments/,
+but are called directly by backend code.
+*/
+package partials
